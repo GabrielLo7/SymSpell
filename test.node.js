110d37ae6b437b912cacb2b761f6e39bf4286747
Dong Yuwei <yuwei.dong@rea-group.com>
2015-04-16 17:07:54 +0800

    test spellcheck for Chinese word combination

--- test.node.js
+++ test.node.js
@@ -1,8 +1,18 @@
 #! /usr/local/bin/node
 var SpellChecker = require('./symspell.js');
 
-var corrector = new SpellChecker(4, 2);
-corrector.addWords('awesome hallelujah different');
+function tokenizer(words){
+    return words.toLowerCase().split(' ');
+}
+
+var maxEditDistance = 4;
+
+var corrector = new SpellChecker(maxEditDistance);
+
+corrector.addWords('awesome hallelujah different 测试 你好', null, tokenizer);
+
 console.log(corrector.lookup('awsom'));
 console.log(corrector.lookup('haleluah'));
 console.log(corrector.lookup('difrent'));
+console.log(corrector.lookup('测'));
+console.log(corrector.lookup('你坏'));
